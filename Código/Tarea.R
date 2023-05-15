3260d8aa48ed5990687b6d5a016d6c79e85737f2
Anahi Canedo <anahi.canedotx@gmail.com>
2023-05-15 00:43:35 -0600

    Tarea: Analizar el DataPaper de polinizadores

---
+++ Código/Tarea.R
@@ -0,0 +1,15 @@
+###############################################################################
+# PRACTICA
+
+# Este es un Data Paper https://db.cger.nies.go.jp/JaLTER/metacat/metacat/ERDP-2020-01.1/jalter-en 
+# En Sapporo,Japón en 1979 y 1989, se llevó a cabo un monitoreo sistemático de abejas, 
+# en el Jardín Botánico de la Universidad de Hokkaido, el estudio incluía el muestreo 
+# de abejas y el registro de las flores visitadas por las abejas de abril a septiembre. 
+
+## Preguntas: 
+# 1. ¿Qué información se recolectó en el data paper? Explora los encabezados del archivo y describe brevemente.
+# 2. ¿Cuántos ejemplares de abejas se resgristraron?
+# 3. ¿Qué géneros de abejas se encontraron en este estudio?
+# 4. ¿Cuántas flores diferentes visitaron las abejas de este estudio?
+# 5. ¿Cuántos individuos se registraron por cada año? Enlistalos en una tabla con la función as.table()
+
