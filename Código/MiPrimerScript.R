71d5d5a3e5e4dd9fb92da298294212dc79b49b3e
Gabriela <anahi.canedotx@gmail.com>
2023-05-16 14:05:38 -0600

    add mi primer scrip r

---
+++ Código/MiPrimerScript.R
@@ -0,0 +1,49 @@
+# Aqui puedo escribir codigo 
+
+objeto <- 30
+objeto2 <- 20
+
+fruta1 <- "manzana"
+fruta2 <- "pera"
+
+#Esto es una suma
+10+90
+#Una resta
+500-74
+#Una division
+1000/2
+#Exponenciales
+10^2
+# Operaciones anidadas
+(1000/2)*5/7+2
+
+# Guardar opreciones numericas en un objeto
+varA <- (1000/2)*5/7+2
+
+#Imprimir objeto
+print(varA)
+varA
+
+# Enlistar objetos
+ls()
+# Borrar
+rm(varA)
+ls()
+
+#Funciones para hace roperaciones simples
+SUMA <- sum(80+90+71+60)
+
+#
+est.ECOSUR <- c("Nancy","Tony","Luis","Manuel", "Paco", "Denice","Anahí")
+edades.ECOSUR <- c(32,33,28,30,32,27,30)
+estatura.ECOSUR <- c(166,170,165,168,172,151,158)  
+
+estatura.ECOSUR/2
+estatura.ECOSUR*10
+
+GRUPO.GC <- cbind(est.ECOSUR,edades.ECOSUR,estatura.ECOSUR)
+dim(GRUPO.GC)
+
+GRUPO.GC[6, ]
+GRUPO.GC[ ,2]
+
